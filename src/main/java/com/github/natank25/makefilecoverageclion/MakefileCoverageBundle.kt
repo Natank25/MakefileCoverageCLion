ff1a6f172bc04de28ef2a34ee4eaeafe1fa43c41
GitHub Action <action@github.com>
2024-12-21 15:52:08 +0000

    Template cleanup

--- src/main/java/com/github/natank25/makefilecoverageclion/MakefileCoverageBundle.kt
+++ src/main/java/com/github/natank25/makefilecoverageclion/MakefileCoverageBundle.kt
@@ -1,13 +1,13 @@
-package org.jetbrains.plugins.template
+package com.github.natank25.makefilecoverageclion
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.MakefileCoverageBundle"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object MakefileCoverageBundle : DynamicBundle(BUNDLE) {
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
